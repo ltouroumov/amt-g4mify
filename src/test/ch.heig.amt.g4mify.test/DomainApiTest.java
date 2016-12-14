7a9b2b82d5b47484be03eb4e35259262be93cc5e
lepouletsuisse <xxpoulletxx@gmail.com>
2016-12-14 13:58:26 +0100

    Fix HttpTestRequest to process all errors

--- src/test/ch.heig.amt.g4mify.test/DomainApiTest.java
+++ src/test/ch.heig.amt.g4mify.test/DomainApiTest.java
@@ -6,17 +6,15 @@ import ch.heig.amt.g4mify.model.Domain;
 import com.google.gson.Gson;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.FixMethodOrder;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runners.MethodSorters;
+import org.junit.rules.TestName;
 
 import java.util.HashMap;
-import java.util.Map;
 
-import static ch.heig.amt.g4mify.Utils.HttpTestRequest.is500;
+import static ch.heig.amt.g4mify.Utils.HttpTestRequest.isError;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.fail;
 
 /**
  * Created by Le Poulet Suisse on 05.12.2016.
@@ -24,13 +22,17 @@ import static org.junit.Assert.fail;
 public class DomainApiTest {
     private Domain testDomain = null;
 
+    @Rule
+    public TestName name = new TestName();
+
     @Before
     public void init(){
+        System.out.println("-- " + name.getMethodName() + " --");
         System.out.println("- INITIALISATION");
         HttpTestRequest request = new HttpTestRequest();
         Gson gson = new Gson();
         TestResponse response = request.test("/register", "{\"name\": \"TestDomain\"}", null, "POST");
-        if(is500(response)) return;
+        if(isError(response)) return;
         testDomain = gson.fromJson(response.getBody(), Domain.class);
         System.out.println("Name: " + testDomain.getName() + " // Id: " + testDomain.getId() + " // Key: " + testDomain.getKey());
         assertEquals(201, response.getStatusCode());
@@ -44,7 +46,7 @@ public class DomainApiTest {
         HashMap<String, String> headers = new HashMap<>();
         headers.put("identity", testDomain.getId() + ":" + testDomain.getKey());
         TestResponse response = request.test("/api/domain", null, headers, "GET");
-        if(is500(response)) return;
+        if(isError(response)) return;
         Domain domain = gson.fromJson(response.getBody(), Domain.class);
         System.out.println("Name: " + domain.getName() + " // Id: " + domain.getId() + " // Key: " + domain.getKey());
         assertEquals(response.getStatusCode(), 200);
@@ -60,7 +62,7 @@ public class DomainApiTest {
         HashMap<String, String> headers = new HashMap<>();
         headers.put("identity", testDomain.getId() + ":" + testDomain.getKey());
         TestResponse response = request.test("/api/domain", "{\"name\": \"This domain name has changed\"}", headers, "PUT");
-        if(is500(response)) return;
+        if(isError(response)) return;
         Domain domain = gson.fromJson(response.getBody(), Domain.class);
         System.out.println("Name: " + domain.getName() + " // Id: " + domain.getId() + " // Key: " + domain.getKey());
         assertEquals(200, response.getStatusCode());
@@ -76,11 +78,12 @@ public class DomainApiTest {
         HashMap<String, String> headers = new HashMap<>();
         headers.put("identity", testDomain.getId() + ":" + testDomain.getKey());
         TestResponse response = request.test("/api/domain", null, headers, "DELETE");
-        if(is500(response)) return;
+        if(isError(response)) return;
         String responseBody = response.getBody();
         if(response.getStatusCode() == 200){
-            System.out.print("Correctly deleted domain " + testDomain.getId());
+            System.out.println("Correctly deleted domain " + testDomain.getId());
         }
+        System.out.println("\n");
         assertEquals(200, response.getStatusCode());
     }
 }
