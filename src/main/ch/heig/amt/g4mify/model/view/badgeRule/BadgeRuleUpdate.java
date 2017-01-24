fb3e410086dbc3497be06b187da0ce1af2014fff
Laureline David <ltouroumov@gmail.com>
2017-01-24 17:11:47 +0100

    BadgeRulesApi request body now takes id-less types

---
+++ src/main/ch/heig/amt/g4mify/model/view/badgeRule/BadgeRuleUpdate.java
@@ -0,0 +1,14 @@
+package ch.heig.amt.g4mify.model.view.badgeRule;
+
+/**
+ * Created by Frederic on 14.12.16.
+ */
+public class BadgeRuleSummary {
+
+    public long id;
+
+    public String condition;
+
+    public String grants;
+
+}
